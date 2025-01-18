5f711b0e2afbfe095536ec092a5ce9b87dcd19b5
yusuf <ysfnc27@gmail.com>
2025-01-18 22:58:17 +0300

    routing

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,9 +3,14 @@ var router = express.Router();
 
 const config = require("../config");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'yusuf', config:config});
-});
+const fs = require("fs");
+
+let routes = fs.readdirSync(__dirname);
+
+for(let route of routes){
+  if(route.includes(".js") && route != "index.js"){
+    router.use("/" + route.replace(".js", ""), require("./" + route));
+  }
+}
 
 module.exports = router;
